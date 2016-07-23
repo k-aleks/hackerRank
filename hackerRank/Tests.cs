134c439a1d6a1c1116a96f7ce2741dcfce3355ab
kazakov <kazakov@skbkontur.ru>
2016-07-23 19:38:32 +0500

    Quicksort In-Place -- begin

--- hackerRank/Tests.cs
+++ hackerRank/Tests.cs
@@ -11,16 +11,5 @@ namespace hackerRank
 	[TestFixture]
 	class Solution_Tests
 	{
-		[TestCase(0, new int[] {1})]
-		[TestCase(1, 1, 1, 1)]
-		[TestCase(3, 1, 1, 2, 3, 2, 1, 1)]
-		[TestCase(2, 1, 4, 2, 1, 1, 1, 2)]
-		[TestCase(-1, 1, 4, 1, 1, 1, 2)]
-		public void Test(int expectedMid, params int[] arr)
-		{
-			var res = new Solution.MidFinder().FindMid(arr);
-
-			res.Should().Be(expectedMid);
-		}
 	}
 }
