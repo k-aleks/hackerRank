f1d3e13bf52ace85c3c90f7061490f6f7c5bc582
kazakov <kazakov@skbkontur.ru>
2016-07-24 12:09:21 +0500

    Snakes and Ladders: The Quickest Way Up -- 1st try, 4/7 done

--- hackerRank/Tests.cs
+++ hackerRank/Tests.cs
@@ -1,15 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using FluentAssertions;
 using NUnit.Framework;
 
 namespace hackerRank
 {
 	[TestFixture]
-	class Solution_Tests
+	internal class Solution_Tests
 	{
+		[Test]
+		public void Test_1()
+		{
+			var ladders = new Dictionary<int, int>
+			{
+				{32, 62},
+				{42, 68},
+				{12, 98}
+			};
+			var snakes = new Dictionary<int, int>
+			{
+				{95, 13},
+				{97, 25},
+				{93, 37},
+				{79, 27},
+				{75, 19},
+				{49, 47},
+				{67, 17},
+			};
+			var pathFinder = new Solution.ShortestPathFinder(ladders, snakes);
+			var res = pathFinder.FindPath();
+
+			res.Should().Be(3);
+		}
+
+		[Test]
+		public void Test_2()
+		{
+			var ladders = new Dictionary<int, int>
+			{
+				{8,52},
+				{6,80},
+				{26,42},
+				{2,72},
+			};
+			var snakes = new Dictionary<int, int>
+			{
+				{51,19},
+				{39,11},
+				{37,29},
+				{81,3},
+				{59,5},
+				{79,23},
+				{53,7},
+				{43,33},
+				{77,21},
+			};
+			var pathFinder = new Solution.ShortestPathFinder(ladders, snakes);
+			var res = pathFinder.FindPath();
+
+			res.Should().Be(5);
+		}
 	}
 }
