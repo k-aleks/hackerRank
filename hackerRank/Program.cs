24bca7be47017419af36a63759528044927bebe1
kazakov <kazakov@skbkontur.ru>
2016-08-07 14:42:23 +0500

    cleanup

--- hackerRank/Program.cs
+++ hackerRank/Program.cs
@@ -6,48 +6,6 @@ internal class Solution
 {
 	private static void Main(String[] args)
 	{
-		Console.SetIn(File.OpenText(@"C:\dev\hackerRank\hackerRank\testData\StringSimilarity\case10input"));
-		int testCasesCount = int.Parse(Console.ReadLine());
-		string[] testCases = new string[testCasesCount];
-		for (int i = 0; i < testCasesCount; i++)
-		{
-			testCases[i] = Console.ReadLine();
-		}
-		foreach (var testCase in testCases)
-		{
-			var res = StringSimularity(testCase);
-			Console.Out.WriteLine(res);
-		}
 	}
 
-	private static long StringSimularity(string testCase)
-	{
-		long res = GetZFunction(testCase).Sum(i => (long) i);
-		return res;
-	}
-
-	public static int[] GetZFunction(string s)
-	{
-		int[] z = new int[s.Length];
-		if (s.Length == 0)
-			return z;
-		z[0] = s.Length;
-		int n = s.Length;
-		int l = 0, r = 0;
-		for (int i = 1; i < n; i++)
-		{
-			if (i <= r)
-			{
-				z[i] = Math.Min(r - i + 1, z[i - l]);
-			}
-			while (i + z[i] < n && s[z[i]] == s[i + z[i]])
-				z[i]++;
-			if (i + z[i] - 1 > r)
-			{
-				r = i + z[i] - 1;
-				l = i;
-			}
-		}
-		return z;
-	}
 }
--- hackerRank/Tests.cs
+++ hackerRank/Tests.cs
@@ -8,18 +8,5 @@ namespace hackerRank
 	[TestFixture]
 	internal class Solution_Tests
 	{
-		[TestCase("abab", new int[]{4,0,2,0})]
-		[TestCase("ababa", new int[]{5,0,3,0,1})]
-		[TestCase("aaabbb", new int[]{6,2,1,0,0,0})]
-		[TestCase("ababaa", new int[] {6,0,3,0, 1,1})]
-		[TestCase("", new int[] {})]
-		[TestCase("a", new int[] {1})]
-		[TestCase("aa", new int[] {2,1})]
-		[TestCase("aaa", new int[] {3,2,1})]
-		public void Test(string s, int[] expectedResult)
-		{
-			var res = Solution.GetZFunction(s);
-			res.Should().Equal(expectedResult);
-		}
 	}
 }
