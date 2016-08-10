319ad7b179ed769230b57769b385fad181c59d13
kazakov <kazakov@skbkontur.ru>
2016-08-10 13:32:16 +0500

    Done 4/20

--- hackerRank/Program.cs
+++ hackerRank/Program.cs
@@ -7,6 +7,79 @@ internal class Solution
 {
 	private static void Main(String[] args)
 	{
+		int n = int.Parse(Console.ReadLine());
+		var persons = new List<Person>();
+		for (int i = 0; i < n; i++)
+		{
+			var personPassions = Console.ReadLine().Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries).Skip(2).ToArray();
+			persons.Add(new Person(personPassions));
+		}
+		var events = new List<Event>();
+		for (int i = 0; i < n; i++)
+		{
+			var eventPassions = Console.ReadLine().Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries).Skip(1).ToArray();
+			events.Add(new Event(eventPassions));
+		}
+
+		int currentDepth = 0;
+		int maxDepth = 0;
+		FindMaxHappy(persons, events, ref currentDepth, ref maxDepth);
+
+		Console.Out.WriteLine(maxDepth);
 	}
 
+	public static void FindMaxHappy(List<Person> persons, List<Event> events, ref int currentDepth, ref int maxDepth)
+	{
+		foreach (var person in persons.Where(p => !p.Taken))
+		{
+			person.Taken = true;
+			List<Event> possibleEvents = GetPossibleEvents(person, events);
+			foreach (var ev in possibleEvents)
+			{
+				ev.Taken = true;
+				currentDepth++;
+				if (maxDepth < currentDepth)
+					maxDepth = currentDepth;
+				FindMaxHappy(persons, events, ref currentDepth, ref maxDepth);
+				currentDepth--;
+				ev.Taken = false;
+			}
+			person.Taken = false;
+		}
+	}
+
+	private static List<Event> GetPossibleEvents(Person person, List<Event> events)
+	{
+		var res = new List<Event>();
+		foreach (var ev in events)
+		{
+			if (!ev.Taken && person.Passions.Any(passion => ev.Passions.Contains(passion)))
+			{
+				res.Add(ev);
+			}
+		}
+		return res;
+	}
+}
+
+class Event
+{
+	public Event(params string[] passions)
+	{
+		Passions = new HashSet<string>(passions);
+	}
+
+	public bool Taken = false;
+	public HashSet<string> Passions;
+}
+
+class Person
+{
+	public Person(params string[] passions)
+	{
+		Passions = new List<string>(passions);
+	}
+
+	public bool Taken = false;
+	public List<string> Passions;
 }
--- hackerRank/Tests.cs
+++ hackerRank/Tests.cs
@@ -8,5 +8,101 @@ namespace hackerRank
 	[TestFixture]
 	internal class Solution_Tests
 	{
+		[Test]
+		public void Test1()
+		{
+			List<Person> p = new List<Person>()
+			{
+				new Person("p1"),
+				new Person("p2"),
+				new Person("p3"),
+			};
+
+			List<Event> e = new List<Event>()
+			{
+				new Event("p1"),
+				new Event("p2"),
+				new Event("p3"),
+			};
+
+			int currentDepth = 0;
+			int maxDepth = 0;
+			Solution.FindMaxHappy(p, e, ref currentDepth, ref maxDepth);
+
+			maxDepth.Should().Be(3);
+		}
+
+		[Test]
+		public void Test2()
+		{
+			List<Person> p = new List<Person>()
+			{
+				new Person("p1"),
+				new Person("p2"),
+				new Person("p3"),
+			};
+
+			List<Event> e = new List<Event>()
+			{
+				new Event("p1"),
+				new Event("p1"),
+				new Event("p1"),
+			};
+
+			int currentDepth = 0;
+			int maxDepth = 0;
+			Solution.FindMaxHappy(p, e, ref currentDepth, ref maxDepth);
+
+			maxDepth.Should().Be(1);
+		}
+
+		[Test]
+		public void Test3()
+		{
+			List<Person> p = new List<Person>()
+			{
+				new Person("p1"),
+				new Person("p2"),
+				new Person("p3"),
+			};
+
+			List<Event> e = new List<Event>()
+			{
+				new Event("p2"),
+				new Event("p2"),
+				new Event("p2"),
+			};
+
+			int currentDepth = 0;
+			int maxDepth = 0;
+			Solution.FindMaxHappy(p, e, ref currentDepth, ref maxDepth);
+
+			maxDepth.Should().Be(1);
+		}
+
+		[Test]
+		public void Test4()
+		{
+			List<Person> p = new List<Person>()
+			{
+				new Person("p1"),
+				new Person("p2"),
+				new Person("p3"),
+			};
+
+			List<Event> e = new List<Event>()
+			{
+				new Event("p3"),
+				new Event("p2"),
+				new Event("p1"),
+			};
+
+			int currentDepth = 0;
+			int maxDepth = 0;
+			Solution.FindMaxHappy(p, e, ref currentDepth, ref maxDepth);
+
+			maxDepth.Should().Be(3);
+		}
+
 	}
 }
