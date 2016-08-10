e59f4c9c447b7907c60f57b6f595b46dbc352b03
kazakov <kazakov@skbkontur.ru>
2016-08-10 11:11:07 +0500

    Cleanup and add time converter

--- hackerRank/Program.cs
+++ hackerRank/Program.cs
@@ -7,96 +7,13 @@ internal class Solution
 {
 	private static void Main(String[] args)
 	{
-		var x = Int32.Parse(Console.ReadLine());
-		for (int i = 0; i < x; i++)
-		{
-			var ratingsCount = Int32.Parse(Console.ReadLine());
-			bool res = AnalizeRatings(ratingsCount);
-			if (res)
-				Console.Out.WriteLine("ORDER EXISTS");
-			else
-				Console.Out.WriteLine("ORDER VIOLATION");
-		}
 	}
 
-	private static bool AnalizeRatings(int ratingsCount)
+	public static DateTime UnixTimeStampToDateTime( double unixTimeStamp )
 	{
-		var graph = new Dictionary<string, Node>();
-		for (int i = 0; i < ratingsCount; i++)
-		{
-			string[] attractions = Console.ReadLine().Split(',');
-			for (int j = 0; j < attractions.Length - 1; j++)
-			{
-				var curr = attractions[j];
-				var next = attractions[j + 1];
-				EnsureExistsInDic(graph, curr);
-				EnsureExistsInDic(graph, next);
-				AddChild(graph, curr, next);
-			}
-		}
-		List<Node> rootNodes = graph.Where(pair => !pair.Value.HasParent).Select(pair => pair.Value).ToList();
-		foreach (var rootNode in rootNodes)
-		{
-			if (FindCycle(rootNode, graph))
-			{
-				return false;
-			}
-		}
-		if (graph.Count > 0)
-			return false;
-		return true;
+	    // Unix timestamp is seconds past epoch
+	    System.DateTime dtDateTime = new DateTime(1970,1,1,0,0,0,0,System.DateTimeKind.Utc);
+	    dtDateTime = dtDateTime.AddSeconds( unixTimeStamp ).ToLocalTime();
+	    return dtDateTime;
 	}
-
-	public static bool FindCycle(Node node, Dictionary<string, Node> graph)
-	{
-		graph.Remove(node.Key);
-
-		if (!node.IsWhite)
-			return true;
-		if (node.Children == null)
-			return false;
-		node.IsWhite = false;
-		foreach (var child in node.Children)
-		{
-			if (FindCycle(child, graph))
-				return true;
-		}
-		node.IsWhite = true;
-		return false;
-	}
-
-	private static void AddChild(Dictionary<string, Node> graph, string parentKey, string childKey)
-	{
-		var parent = graph[parentKey];
-		var child = graph[childKey];
-		child.HasParent = true;
-		parent.AddChild(child);
-	}
-
-	private static void EnsureExistsInDic(Dictionary<string, Node> graph, string key)
-	{
-		if (!graph.ContainsKey(key))
-			graph.Add(key, new Node(key));
-	}
-
 }
-
-	internal class Node
-	{
-		public Node(string key)
-		{
-			Key = key;
-		}
-
-		public string Key;
-		public HashSet<Node> Children;
-		public bool IsWhite = true;
-		public bool HasParent = false;
-
-		public void AddChild(Node child)
-		{
-			if (Children == null)
-				Children = new HashSet<Node>();
-			Children.Add(child);
-		}
-	}
--- hackerRank/Tests.cs
+++ hackerRank/Tests.cs
@@ -9,48 +9,11 @@ namespace hackerRank
 	internal class Solution_Tests
 	{
 		[Test]
-		public void Test1()
+		public void TestUnixTimeConverter()
 		{
-			var node = new Node("1");
-			var res = Solution.FindCycle(node, new Dictionary<string, Node>());
-			res.Should().BeFalse();
-		}
+			var dt = Solution.UnixTimeStampToDateTime(1467720000);
+			dt.Date.Should().Be(new DateTime(2016, 07, 05));
 
-		[Test]
-		public void Test2()
-		{
-			var node1 = new Node("1");
-			var node2 = new Node("2");
-			var node3 = new Node("3");
-			node1.AddChild(node2);
-			node1.AddChild(node3);
-			var res = Solution.FindCycle(node1, new Dictionary<string, Node>());
-			res.Should().BeFalse();
 		}
-
-		[Test]
-		public void Test3()
-		{
-			var node1 = new Node("1");
-			var node2 = new Node("2");
-			node1.AddChild(node2);
-			node2.AddChild(node1);
-			var res = Solution.FindCycle(node1, new Dictionary<string, Node>());
-			res.Should().BeTrue();
-		}
-
-		[Test]
-		public void Test4()
-		{
-			var node1 = new Node("1");
-			var node2 = new Node("2");
-			var node3 = new Node("3");
-			node1.AddChild(node2);
-			node2.AddChild(node3);
-			node3.AddChild(node1);
-			var res = Solution.FindCycle(node1, new Dictionary<string, Node>());
-			res.Should().BeTrue();
-		}
-
 	}
 }
