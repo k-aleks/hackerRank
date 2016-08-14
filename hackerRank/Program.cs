952f4faacd1d7d6e65d1223848c7e51437206d54
kazakov <kazakov@skbkontur.ru>
2016-08-14 18:29:04 +0500

    Playng with insertion sort

--- hackerRank/Program.cs
+++ hackerRank/Program.cs
@@ -1,19 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 internal class Solution
 {
 	private static void Main(String[] args)
 	{
-		int valueToSearch = Int32.Parse(Console.ReadLine());
 		int araySize = Int32.Parse(Console.ReadLine());
-		var arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray() ;
+		int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
 		InsertionSort(arr);
-
 	}
 
-	public static void InsertionSort(int[] arr)
+	public static void InsertionSort(int[] ar)
 	{
+		int shift = 0;
+		for (int i = 1; i < ar.Length; i++)
+		{
+			InsertionSort(ar, i, ref shift);
+			//Print(ar);
+		}
+		Console.Out.WriteLine(shift);
+	}
+
+	public static void InsertionSort(int[] ar, int elementIndex, ref int shift)
+	{
+		int elementToInsert = ar[elementIndex];
+		for (int i = elementIndex - 1; i >= 0; i--)
+		{
+			if (ar[i] > elementToInsert)
+			{
+				ar[i + 1] = ar[i];
+				shift++;
+			}
+			else
+			{
+				ar[i + 1] = elementToInsert;
+				return;
+			}
+		}
+		ar[0] = elementToInsert;
+	}
+
+	public static void Print(int[] ar)
+	{
+		Console.Out.WriteLine(string.Join(" ", ar));
 	}
 }
--- hackerRank/Tests.cs
+++ hackerRank/Tests.cs
@@ -8,7 +8,53 @@ namespace hackerRank
 	[TestFixture]
 	internal class Solution_Tests
 	{
+		[Test]
+		public void Test_1()
+		{
+			var ar = new int[]{1};
+			Solution.InsertionSort(ar);
+			ar.Should().Equal(1);
+		}
 
+		[Test]
+		public void Test_2()
+		{
+			var ar = new int[]{1, 2};
+			Solution.InsertionSort(ar);
+			ar.Should().Equal(1, 2);
+		}
+
+		[Test]
+		public void Test_3()
+		{
+			var ar = new int[]{1,3,2};
+			Solution.InsertionSort(ar);
+			ar.Should().Equal(1, 2, 3);
+		}
+
+		[Test]
+		public void Test_4()
+		{
+			var ar = new int[]{1, 1};
+			Solution.InsertionSort(ar);
+			ar.Should().Equal(1, 1);
+		}
+
+		[Test]
+		public void Test_5()
+		{
+			var ar = new int[]{2, 1};
+			Solution.InsertionSort(ar);
+			ar.Should().Equal(1, 2);
+		}
+
+		[Test]
+		public void Test_6()
+		{
+			var ar = new int[]{4, 5, 0, 3, 2, 1};
+			Solution.InsertionSort(ar);
+			ar.Should().Equal(0,1,2,3,4,5);
+		}
 
 	}
 }
