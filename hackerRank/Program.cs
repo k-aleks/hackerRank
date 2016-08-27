44c4135aaa66c70347929476640a41a8a12bc1f6
kazakov <kazakov@skbkontur.ru>
2016-08-27 12:29:10 +0500

    Sorting - Find the Median - 100%

--- hackerRank/Program.cs
+++ hackerRank/Program.cs
@@ -6,10 +6,60 @@ internal class Solution
 	private static void Main(String[] args)
 	{
 		int araySize = Int32.Parse(Console.ReadLine());
-
+		var arr = Console.ReadLine().Split(' ').Select(Int32.Parse).ToArray();
+		int median = FindTheMedian(arr);
+		Console.Out.WriteLine(median);
 	}
 
-	
+	public static int FindTheMedian(int[] arr)
+	{
+		int medianIndex = arr.Length/2;
+		int left = 0;
+		int right = arr.Length - 1;
+
+		while (true)
+		{
+			int pivot = arr[right];
+			int firstThatBigger = -1;
+			for (int i = left; i <= right; i++)
+			{
+				if (arr[i] > pivot && firstThatBigger == -1)
+				{
+					firstThatBigger = i;
+				}
+				else if (arr[i] <= pivot && firstThatBigger >= 0)
+				{
+					Swap(arr, i, firstThatBigger);
+					firstThatBigger++;
+				}
+			}
+			int pivotIndex;
+			if (firstThatBigger >= 0)
+				pivotIndex = firstThatBigger - 1;
+			else
+				pivotIndex = right;
+
+			if (pivotIndex == medianIndex)
+				return pivot;
+
+			if (pivotIndex < medianIndex)
+			{
+				left = pivotIndex + 1;
+			}
+			else
+			{
+				right = pivotIndex - 1;
+			}
+		}
+	}
+
+	private static void Swap(int[] arr, int index1, int index2)
+	{
+		int tmp = arr[index1];
+		arr[index1] = arr[index2];
+		arr[index2] = tmp;
+	}
+
 
 	public static void Print<T>(T[] ar)
 	{
--- hackerRank/Tests.cs
+++ hackerRank/Tests.cs
@@ -8,6 +8,12 @@ namespace hackerRank
 	[TestFixture]
 	internal class Solution_Tests
 	{
-
+		[TestCase(new []{1}, 1)]
+		[TestCase(new []{1, 2, 3}, 2)]
+		public void Test(int[] arr, int expectedMedian)
+		{
+			var res = Solution.FindTheMedian(arr);
+			res.Should().Be(expectedMedian);
+		}
 	}
 }
