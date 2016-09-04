b7bb78acd3c0fe43c6d6ac211f24a404fa44b8c7
kazakov <kazakov@skbkontur.ru>
2016-09-04 18:34:42 +0500

    cleanup

--- hackerRank/Program.cs
+++ hackerRank/Program.cs
@@ -9,61 +9,6 @@ internal class Solution
 
 		for (int i = 0; i < experimentsCount; i++)
 		{
-			int araySize = Int32.Parse(Console.ReadLine());
-			int[] arr = Console.ReadLine().Split(' ').Select(Int32.Parse).ToArray();
-
-			long shifts = MergeSort.Sort(arr);
-			Console.Out.WriteLine(shifts);
-		}
-	}
-
-	public static void Print<T>(T[] ar)
-	{
-		Console.Out.WriteLine(string.Join(" ", ar));
-	}
-}
-
-public static class MergeSort
-{
-	public static long Sort(int[] arr)
-	{
-		long shifts = 0;
-		Sort(arr, 0, arr.Length - 1, new int[arr.Length], ref shifts);
-		return shifts;
-	}
-
-	private static void Sort(int[] arr, int left, int right, int[] tmp, ref long shifts)
-	{
-		if (left >= right)
-			return;
-		int mid = (right + left)/2;
-		Sort(arr, left, mid, tmp, ref shifts);
-		Sort(arr, mid + 1, right, tmp, ref shifts);
-
-		Merge(arr, left, mid, right, tmp, ref shifts);
-	}
-
-	private static void Merge(int[] arr, int left, int mid, int right, int[] tmp, ref long shifts)
-	{
-		Array.Copy(arr, left, tmp, left, right - left + 1);
-		int i = left;
-		int j = mid + 1;
-		int iter = 0;
-		while (i <= mid && j <= right)
-		{
-			if (tmp[i] <= tmp[j])
-				arr[left + iter++] = tmp[i++];
-			else
-			{
-				arr[left + iter] = tmp[j];
-				shifts += j - (left + iter);
-				iter++;
-				j++;
-			}
-		}
-		if (i <= mid)
-		{
-			Array.Copy(tmp, i, arr, left + iter, mid - i + 1);
 		}
 	}
 }
--- hackerRank/Tests.cs
+++ hackerRank/Tests.cs
@@ -7,18 +7,6 @@ namespace hackerRank
 	[TestFixture]
 	internal class Solution_Tests
 	{
-		[TestCase(new[] {2, 1, 3, 1, 2}, 4)]
-		[TestCase(new int[] {}, 0)]
-		[TestCase(new[] {1}, 0)]
-		[TestCase(new[] {1, 1, 2, 2, 3, 3, 5, 5, 7, 7, 9, 9}, 0)]
-		[TestCase(new[] {10, 9, 8, 7, 6, 5, 4, 3, 2, 1}, 45)]
-		public void Test(int[] arr, int expectedShifts)
-		{
-			long shifts = MergeSort.Sort(arr);
-			arr.Should().BeInAscendingOrder();
-			shifts.Should().Be(expectedShifts);
-		}
-
 	
 	}
 }
