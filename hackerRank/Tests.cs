fe02d683f5b81ce71a44e31276a5c29ff607e1d2
Aleksandr Kazakov <kazakov.dev@gmail.com>
2017-03-16 19:15:34 +0500

    cleanup

--- hackerRank/Tests.cs
+++ hackerRank/Tests.cs
@@ -14,54 +14,9 @@ namespace hackerRank
         [Test]
         public void Test()
         {
-            var n = new Node(3,
-                new Node(5, new Node(1, null, null), new Node(4, null, null)),
-                new Node(3, new Node(6, null, null), null));
-            preOrder(n);
         }
 
 
-        public void preOrder(Node root)
-        {
-            LinkedList<Node> q = new LinkedList<Node>();
-            preOrder(root, q);
-        }
-
-        public void preOrder(Node n, LinkedList<Node> q)
-        {
-            Console.Out.Write(n.data + " ");
-            q.AddLast(n);
-            if (n.left != null)
-                preOrder(n.left, q);
-            else
-            {
-                Node fromQ;
-                do
-                {
-                    if (q.Count == 0)
-                        return;
-
-                    fromQ = q.Last();
-                    q.RemoveLast();
-                } while (fromQ.right == null);
-                preOrder(fromQ.right);
-            }
-
-        }
-
-        internal class Node
-        {
-            public Node(int data, Node left, Node right)
-            {
-                this.data = data;
-                this.left = left;
-                this.right = right;
-            }
-
-            public int data;
-            public Node left;
-            public Node right;
-        }
 
 
     }
