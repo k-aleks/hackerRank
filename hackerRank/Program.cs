94513ea153732eb0f102d52e2ccc45de9c6963fc
Aleksandr Kazakov <kazakov.dev@gmail.com>
2017-03-17 10:13:28 +0500

    Implementing binary heap. TreeIndexer completed

--- hackerRank/Program.cs
+++ hackerRank/Program.cs
@@ -9,6 +9,66 @@ internal class Solution
 	    int count = Int32.Parse(Console.ReadLine());
 
 	}
+}
 
+public class BinaryHeap
+{
+    List<int> list = new List<int>();
+
+    public void Add(int newElement)
+    {
+        list.Add(newElement);
+        UpHeapLastElement();
+    }
+
+    private void UpHeapLastElement()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class BinaryTreeIndexer
+{
+    public int GetParentIndex(int childIndex)
+    {
+        int bucketNumber = GetBucketNumber(childIndex);
+        int indexInBucket = GetIndexInBucket(childIndex, bucketNumber);
+        int parentBucketSize = GetBucketSize(bucketNumber - 1);
+        int indexInBucketOfParent = (int) indexInBucket / 2;
+        return GetBucketStartIndex(bucketNumber - 1) + indexInBucketOfParent;
+    }
+
+    public int GetLeftChildIndex(int parentIndex)
+    {
+        int bucketNumber = GetBucketNumber(parentIndex);
+        int indexInBucket = GetIndexInBucket(parentIndex, bucketNumber);
+        return GetBucketStartIndex(bucketNumber + 1) + 2 * (indexInBucket);
+    }
+
+    public int GetRightChildIndex(int parentIndex)
+    {
+        return GetLeftChildIndex(parentIndex) + 1;
+    }
+
+    public int GetBucketNumber(int index)
+    {
+        return (int)Math.Log(index + 1) + 1;
+    }
+
+    public int GetIndexInBucket(int index, int bucketNumber)
+    {
+        return index - GetBucketStartIndex(bucketNumber);
+    }
+
+    public int GetBucketStartIndex(int bucketNumber)
+    {
+
+        return (int)Math.Pow(2, bucketNumber) - 1;
+    }
+
+    public int GetBucketSize(int bucketNumber)
+    {
+        return (int) Math.Pow(2, bucketNumber);
+    }
 }
 
--- hackerRank/Tests.cs
+++ hackerRank/Tests.cs
@@ -13,6 +13,40 @@ namespace hackerRank
     {
     }
 
+    [TestFixture]
+    internal class BinaryTreeIndexer_Tests
+    {
+        private BinaryTreeIndexer indexer = new BinaryTreeIndexer();
+
+        [TestCase(0, 1)]
+        [TestCase(1, 3)]
+        [TestCase(2, 5)]
+        [TestCase(3, 7)]
+        [TestCase(4, 9)]
+        [TestCase(5, 11)]
+        [TestCase(6, 13)]
+        public void Test_GetLeftChildIndex(int parentIndex, int expected)
+        {
+            indexer.GetLeftChildIndex(parentIndex).Should().Be(expected);
+        }
+
+        [TestCase(0, 1)]
+        [TestCase(1, 3)]
+        [TestCase(2, 5)]
+        [TestCase(3, 7)]
+        [TestCase(3, 8)]
+        [TestCase(4, 9)]
+        [TestCase(4, 10)]
+        [TestCase(5, 11)]
+        [TestCase(5, 12)]
+        [TestCase(6, 13)]
+        [TestCase(6, 14)]
+        public void Test_GetParentIndex(int expected, int childIndex)
+        {
+            indexer.GetParentIndex(childIndex).Should().Be(expected);
+        }
+
+    }
 
 
 
