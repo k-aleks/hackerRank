6e7babc147dd553ce758611ad917554eddbfb228
Aleksandr Kazakov <kazakov.dev@gmail.com>
2017-03-24 08:45:08 +0500

    Substring Diff: 100% solved

--- hackerRank/Program.cs
+++ hackerRank/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-class Solution {
 
+class Solution
+{
     static void Main(String[] args)
     {
         int cases = int.Parse(Console.ReadLine());
@@ -19,40 +20,47 @@ class Solution {
         int maxSubstringLength = int.MinValue;
         for (int i=0; i<s1.Length; i++)
         {
-            string tmp = RollTheString(s2, i);
+            string tmp;
+            int localMaxSubstringLength;
 
-            int localMaxSubstringLength = GetMaxSubstringLengthInternal(s1, tmp, maxErrorLevel);
+            tmp = s2.Substring(i);
+
+            localMaxSubstringLength = GetMaxSubstringLengthInternal(s1, tmp, 0, tmp.Length-1, maxErrorLevel);
+            if (maxSubstringLength < localMaxSubstringLength)
+                maxSubstringLength = localMaxSubstringLength;
+
+            if (i == 0)
+                continue;
+
+            tmp = s2.Substring(0, i);
+
+            localMaxSubstringLength = GetMaxSubstringLengthInternal(s1, tmp, s1.Length - tmp.Length, s1.Length - 1, maxErrorLevel);
             if (maxSubstringLength < localMaxSubstringLength)
                 maxSubstringLength = localMaxSubstringLength;
         }
         return maxSubstringLength;
     }
 
-    public static string RollTheString(string s, int i)
-    {
-        return (i > 0) ? s.Substring(i) + s.Substring(0, i) : s;
-    }
-
-    public static int GetMaxSubstringLengthInternal(string s1, string s2, int maxErrorLevel)
+    public static int GetMaxSubstringLengthInternal(string s1, string s2, int from, int to, int maxErrorLevel)
     {
         int maxSubstringLen = 0;
         int[] diff = new int[s1.Length];
-        int j = 0;
-        for (int i=0; i<s1.Length; i++)
+        int j = from;
+        for (int i=from, k=0; i<=to; i++,k++)
         {
-            int delta = (s1[i] == s2[i]) ? 0 : 1;
-            diff[i] = (i == 0) ? delta : diff[i-1] + delta;
+            int delta = (s1[i] == s2[k]) ? 0 : 1;
+            diff[i] = (i == from) ? delta : diff[i-1] + delta;
 
-            int errorLevel = diff[i] - ((j == 0) ? 0 : diff[j-1]);
+            int errorLevel = diff[i] - ((j == from) ? 0 : diff[j-1]);
             if (errorLevel > maxErrorLevel)
             {
-                int currentSubstringLength = i-j;
+                int currentSubstringLength = k-(j-from);
                 if (maxSubstringLen < currentSubstringLength)
                     maxSubstringLen = currentSubstringLength;
                 j++;
             }
         }
-        int finalSubstringLength = s1.Length-j;
+        int finalSubstringLength = s2.Length-(j-from);
         if (maxSubstringLen < finalSubstringLength)
             maxSubstringLen = finalSubstringLength;
         return maxSubstringLen;
--- hackerRank/Tests.cs
+++ hackerRank/Tests.cs
@@ -12,13 +12,6 @@ namespace hackerRank
     [TestFixture]
     internal class Solution_Tests
     {
-        [TestCase("tabriz", "otorin", 2, 4)]
-        public void Test_GetMaxSubstringLengthInternal(string s1, string s2, int maxDixx, int expected)
-        {
-            var res = Solution.GetMaxSubstringLengthInternal(s1, s2, maxDixx);
-            res.Should().Be(expected);
-        }
-
         [TestCase("tabriz", "torino", 2, 4)]
         [TestCase("uvtedmbqjjazboxbephroffzguecvykbhyafuhsohmetnmdgqpihumsmoxirjrupgnocppctglnmbvolxuubohegeofgshufltgguaymvyoxpmdklbgkdkkzscsxcxcslistafhkzoisxkkqdqavjnuommxxteuxzgfvmusxlsqdueejicvbkizfbvodusbvruqxbcodzybsxazdglvxzzgnsudxfrbtqrfooprbdoxnodpblnlrypdobzgqnsigezdpkhkphonxgabnkgjsvypcrcgehiehljruykpduxoesekjfpbfpxqmyknvrjncmcretfcolzuabqzlezdyopnrsqpuhzzkmgchbsiohnydxydeytxntrpnxhatiqukhsflcifiehccbhjfgnrettuyxktgauboqulrhymjnxzqhcqsfdmmhgiepbbrmhznkfnadvlfplhcszuyzotbxkuysbqleqnypigncvicnjirkvdtdikzdyfmzeccuxmtorcaapjrsahkylcmsejsaumgmlhrofrlvkqglxcnrqmebuuuvaxmatkdnqzoxdkoxuqkrhcoqjbvbmgjrzohthnhqaanngsxqephzmqesecfccsakgznnjmhoucjtebvzigoryroprrmiidxyrtpocniqoalxqhloxcdplhuvthqsyjznbqmantrbxgoqgdnzyhvpovndgkxmsszfbaqkoimnyxsbvavxmkxhkzoedijyspvzsjknrcvradybzeanesxxuzvxmqtjabnmlcnayysgjxrfsjrcjaoyuliehuqyyoxpezfgxxhgsihqomhppjyitiyiyikomklvuoxbkemkervhucidnoynxcyflyqleagpqkgmnquueexspnoxqpapdatsfzzihiusagklunvcvaavslybundctzqdzfercjpjieobdemnnxbivroxkfhdvhqztcrlsvakyhchaluknxkpknoyrbyyrbuidvjlknssyurtvelfxfletkssjgnlveiysfkajkedvmxarptqrxpnitltailrgbcjjhoyyefdgrbbxzijryihyucszdlijikkivckzkipzclefeeypbstotofgnrovyczkeucfmlcxqyjumlavxutlqaxvtyixeshykftrmjxrzmihffngmdtssnkmvkrhveqocynehdysdevpqfpkkequfavldkibfokesqcpjhmuvamkxebfodaczprkhbblnrliullczyeotqmuyzvsibslxnatqtfarkltzjbfsyaysitcsfohbnpjzopnxeknrecknphcoerfgpzxroyvmunryapmjglacplehabtlgfczttgyvilikbinxskheksgfmaxvlgayfjfebtgsmoprknxvhzzmyaotblsdallzpdzkscgteuhfiygojbdotdorfalsxjjnnjvtdacharuhxrthmkpbobvrjcooonqqzpuvmjcsffzx",
             "vlceyrlgrugkjoblzfxneijkrhrfgvytmmesgbcfphagtdezgjazvytozaplhfpeqxbzxefoiknedxvongyslxzlkaeisjbvgsnnauhkiivbfqkpkjiyjsyftrjtkykixfdiojerqebljlqqmttlfzdutumbsgvlohimtajbpppqznxyejzutljuuylastgbykmnyzthyqzvrtjceuhcmnzfzxseaxxzuynzxynipeidmzsroyuahxtkubeecikuayybyxrnzyafxgkazrovhgsypljpclpzqnlfojosubpfhgstvpxylxbjoqlhthiljdcsqugbgxyltydujmqcbpuytzqrbjptomiromzybicefazzmmredurkvgdrqzvdtrrsimpdzxsogsvreflykbrmleebvdxmurmpnxypetrviatqddkbdmggarlndiovgqpkutyjhkagmpcyirkrffyyvxagvpeyhhefsgcfiupmaydlcmegrhejkgmqtldhgudrltepagpgpfxoqplooskudszfvqobcckexqserdduxfmgkypgcsohcmfyrxrbvgggnfhehhkeckttxrpgrlxsrxetcdnsqesvifajnjlhtceiickvneiamkxonftamyuollbfzvjpecaquzimjpjckajvfjzcfgbhzqaougndjbjrgbvyhetpaxfjdlnrlmlstdplpyxggbokhsretpbzrddmgbuksexevratqkagtmacfcfelbpprbvtgipntbgtluuccoxpxexnyellnjcshsprjdliepcdsjjqsemkujlnzeiburrzubtfbdkuolqnoejjfkzmgxgebyygajffjttyeqojsyeeadbbyayrskmgajvexyyyxvxtcqlqsrqlelrdeuxidrpiyfjczjlauseyuqzkcyzoytubljggpbstgvcgqehpciurrjjrpxxhvsmdnrezijvqcvyzsubfidltxhijmtaprjsoieoabzxsqzvtexsxgdxbiztnqlrjocrclecpbsaeszgqzooliazpebzelvdmvxvormexinetkbtcxaxokulrboqlmmcrsrviclyxtgaymxhharimlaanfqpshuadxghtevlcajmtmqtyvokjxccieuaxpsmegmhjuaxlclmblbpkabtqrmrspsippxtydrodctnbignjoyacldgtsotovqqeejbqiaskghqfegdijlrvkdecgxzdfjlolbimzialprvsdnjljqltyczvhbtdoeechkosiydsceenaafvdgkgdlidzzoiogvjnafcsymrrkylnqjkpzvnbzgsarorudfryeitybpaqziltyplfepguixbblvzzqrcmyvfjggpsvtdgccqyfyukyxxycnetjnaavehnuehlvtaifykkazkcklfqllxreltmzmxhcqhizjymdezzjlgussulurgykaeevmpmabjjprnpxvnztfgnbqmbhue",
@@ -31,19 +24,5 @@ namespace hackerRank
             var res = Solution.GetMaxSubstringLength(s1, s2, maxDixx);
             res.Should().Be(expected);
         }
-
-        [TestCase("torino", 0, "torino")]
-        [TestCase("torino", 1, "orinot")]
-        [TestCase("torino", 2, "rinoto")]
-        [TestCase("torino", 3, "inotor")]
-        [TestCase("torino", 4, "notori")]
-        [TestCase("torino", 5, "otorin")]
-        public void Test_RollTheString(string s, int index,  string expected)
-        {
-            var res = Solution.RollTheString(s, index);
-            res.Should().Be(expected);
-        }
-
-
     }
 }
